792cdb134094ce0599e9b8a1ac37fa5d01a8da92
vrsyvby@gmail.com <3%@tatime>
2021-09-10 23:07:58 +0530

    Anagrams

---
+++ src/garden/vegetables/Anagarams.java
@@ -0,0 +1,73 @@
+package garden.vegetables;
+import java.util.Scanner;
+
+public class Anagarams {
+
+    static boolean isAnagram(String a, String b) {
+        if(a==null || b==null||a==""||b==""){
+            return false;
+        }else if(a.length()!=b.length()){
+            return false;
+        }else{
+
+            a=a.toLowerCase();
+            b=b.toLowerCase();
+            char[] aCharArray=a.toCharArray();
+            char[] bCharArray=b.toCharArray();
+            String firstString=a;
+            int loopIterator=0;
+            while(loopIterator+1<a.length()){
+                String sample= firstString.substring(loopIterator,loopIterator+1);
+
+                char[] samplecharArray=sample.toCharArray();
+                char samplechar=samplecharArray[0];
+
+                int countA=0;
+                int countB=0;
+                for(int i=0;i<a.length();i++){
+
+                    if(aCharArray[i]==samplechar){
+                        ++countA;
+                    }
+                    if(bCharArray[i]==samplechar){
+
+                        ++countB;
+                    }
+                }
+
+
+
+
+                if(countA!=countB) return false;
+                a= a.replace(sample,"");
+                aCharArray=a.toCharArray();
+                b= b.replace(sample,"");
+                bCharArray=b.toCharArray();
+
+
+                loopIterator++;
+
+            }
+
+            return true;
+
+        }
+
+
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
