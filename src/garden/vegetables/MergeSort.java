28dc4076128cb6a7681d6ddcfa973b5d0c609899
vrsyvby@gmail.com <3%@tatime>
2021-10-24 18:29:03 +0530

    MergeSort

---
+++ src/garden/vegetables/MergeSort.java
@@ -0,0 +1,67 @@
+package garden.vegetables;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+
+
+    public static int[] merge(int[] left, int[] right){
+         int[] result= new int[left.length+right.length];
+         int leftindex=0;
+         int rightindex=0;
+         int index=0;
+         while(leftindex<left.length && rightindex<right.length){
+
+             if(left[leftindex]<right[rightindex]){
+                 result[index]= left[leftindex];
+                 leftindex++;
+
+             }else{
+                 result[index]= right[rightindex];
+                 rightindex++;
+             }
+
+             index++;
+
+         }
+
+         while(leftindex<left.length){
+             result[index]= left[leftindex];
+             index++;
+            leftindex++;
+        }
+
+        while(rightindex<right.length){
+            result[index]= right[rightindex];
+            index++;
+            rightindex++;
+        }
+         return result;
+    }
+
+    public static int[]  mergesort(int [] elements){
+
+        if(elements.length==1){
+           return elements;
+        }
+        int midindex= (elements.length)/2;
+        int[] firsthalf= Arrays.copyOfRange(elements,0, midindex);
+        int[] secondhalf= Arrays.copyOfRange(elements,midindex, elements.length);
+
+       return  merge(mergesort(firsthalf), mergesort(secondhalf));
+
+    }
+
+    public static void main(String[] args){
+
+        int[] elements={99,44,6,2,1,5,63,87,283,4,0};
+
+        elements=  mergesort(elements);
+
+        for(int i=0;i<elements.length;i++){
+            System.out.print(elements[i]+" ");
+        }
+
+    }
+}
